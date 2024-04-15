95760b5b3cedc06b1c0209c3a14926176afa04dc
AngelTravieso <angeltraviesoc@gmail.com>
2024-04-15 14:57:31 -0400

    Receiving Events

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 
     commentsByPostId[req.params.id] = comments;
 
-    await axios.post('localhost:4005/events', {
+    await axios.post('http://localhost:4005/events', {
         type: 'CommentCreated',
         data: {
             id: commentId, content,
@@ -35,6 +35,12 @@ app.post('/posts/:id/comments', async (req, res) => {
 
     res.status(201).send(comments);
 
+});
+
+app.post('/events', (req, res) => {
+    console.log('Received Event', req.body.type);
+
+    res.send({});
 });
 
 app.listen(4001, () => {
